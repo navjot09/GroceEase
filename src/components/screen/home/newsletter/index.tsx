a491f25928bbbdc5076fbdba1005b056086182c8
navjots09 <navjot.singh@theschoolprep.com>
2026-09-12 20:21:33 +0530

    fixes

--- src/components/screen/home/newsletter/index.tsx
+++ src/components/screen/home/newsletter/index.tsx
@@ -7,15 +7,15 @@ export const NewsLetter = async () => {
   return (
     <main className=" newsletter-background">
       <Container>
-        <div className="lg:flex">
+        <div className="flex flex-col lg:flex-row">
           <div className="basis-1/2 order-2">
             <Image
-              className="lg:absolute lg:right-0 float-right"
+              className="mx-auto h-auto w-full max-w-md lg:absolute lg:right-0 lg:mx-0 lg:w-auto lg:max-w-none"
               src={HomeMain}
               alt="grocery-search"
             />
           </div>
-          <div className="basis-1/2 py-12 lg:py-24 gap-6 flex flex-col order-1">
+          <div className="basis-1/2 order-1 flex flex-col gap-6 py-12 lg:py-24">
             <h1 className=" text-4xl lg:text-6xl font-bold color-primary text-center lg:text-start">
               Don’t miss our daily <br className="hidden lg:block" /> amazing
               deals.
@@ -23,7 +23,7 @@ export const NewsLetter = async () => {
             <h5 className=" text-lg lg:text-xl text-[#838383] font-semibold lg:mt-7 lg:mb-12 text-center lg:text-start">
               Save up to 60% off on your first order
             </h5>
-            <div className=" flex bg-[#F3F3F3]  items-center w-full lg:w-2/3">
+            <div className=" flex min-w-0 items-center bg-[#F3F3F3] lg:w-2/3">
               <Image
                 src={Send}
                 width={24}
@@ -32,11 +32,13 @@ export const NewsLetter = async () => {
                 alt="grocery-search"
               />
               <input
-                className="bg-transparent focus:outline-none grow px-2"
+                className="min-w-0 grow bg-transparent px-2 focus:outline-none"
                 placeholder="Enter your email"
               />
-              <div className="bg-[#3BB77E] p-2 py-3">
-                <p className=" text-base font-semibold text-white">Subscribe</p>
+              <div className="shrink-0 bg-[#3BB77E] px-2 py-3 sm:px-3">
+                <p className="whitespace-nowrap text-sm font-semibold text-white sm:text-base">
+                  Subscribe
+                </p>
               </div>
             </div>
           </div>
